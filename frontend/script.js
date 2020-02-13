3786b7e348b3442d7c0dd78d7d4e459139284166
JoBapla <jovanbapla@gmail.com>
2020-02-12 19:11:18 -0800

    make list links

--- frontend/script.js
+++ frontend/script.js
@@ -27,11 +27,21 @@ function item(){
         let x=document.getElementById("bar").value;
         document.getElementById("bar").value="";
         bank.push(x);
-        var node = document.createElement("LI");                 
-        var textnode = document.createTextNode(bank.length+". "+bank[bank.length-1]);         
-        node.appendChild(textnode);                              
-        document.getElementById("bank").appendChild(node);
-        document.getElementById("bar").placeholder= "add item"  
+        
+        var link= document.createElement("A");
+        link.setAttribute("href",href="index.html");
+        link.setAttribute('id',bank.length);
+        link.innerHTML="<li>"+bank.length+". "+bank[bank.length-1]+"</li>";
+        document.getElementById("bank").appendChild(link);
+        
+        // var node = document.createElement("LI");  
+        // node.setAttribute('id',bank.length);               
+        // var textnode = document.createTextNode(bank.length+". "+bank[bank.length-1]);         
+        // node.appendChild(textnode);                              
+        // document.getElementById("bank").appendChild(node);
+        
+        // document.getElementById("bar").placeholder= "add item";
+        // console.log(document.getElementById("2"));  
     }
     else{
         document.getElementById("bar").placeholder= "ENTER VALUE!";
